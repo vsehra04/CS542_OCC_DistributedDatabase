d549c9f33a9bb2fbe8a873d4da3a03d76a0d7c22
vsehra04 <vsehra@purdue.edu>
2022-03-03 12:40:46 -0500

    dcg, tm added

--- src/main/java/occ/TransactionManager.java
+++ src/main/java/occ/TransactionManager.java
@@ -1,4 +1,13 @@
 package occ;
 
+import java.util.Set;
+
 public class TransactionManager {
+
+    private DynamicConflictGraph dcg;
+
+    // current running transactions
+    private Set<Transaction> currentTransactions; // Don't think thread-safe needed (confirm later)
+
+
 }
